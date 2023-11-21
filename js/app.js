45cc1e1e3f956b4424a0babdd4b5524423679054
Robinson Andres Cortes RIWI <147889972+robinson-riwi@users.noreply.github.com>
2023-11-20 20:16:49 -0500

    update js 3

--- js/app.js
+++ js/app.js
@@ -1,29 +1,4 @@
-/*
-
-function mostrarFlores(){
-   document.getElementById('abuelita').classList.add('display-none');
-   document.getElementById('flores').classList.remove('display-none');
-   
-}
-function mostrarAbuelita(){
-   document.getElementById('flores').classList.add('display-none');
-   document.getElementById('abuelita').classList.remove('display-none');
-}
-*/
-
-
-/*
-function mostrarFlores(){
-    let urlImagen = "./images/imagen-1.jpg";
-    document.getElementById('flores').setAttribute('src', urlImagen);
-}
-function mostrarAbuelita(){
-    let urlImagen = "./images/imagen-2.jpg";
-    document.getElementById('flores').setAttribute('src', urlImagen);
-}*/
-
-
-function actualizarColor(){
-    let color = document.getElementById('color').value;
-    console.log(color);
+function enviarTexto(){
+     let texto = document.getElementById('texto').value;
+     console.log(texto);
 }
