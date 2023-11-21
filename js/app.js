0f0350a01fc04493c999b1a68f85ca600a3bcfad
Robinson Andres Cortes RIWI <147889972+robinson-riwi@users.noreply.github.com>
2023-11-20 19:06:24 -0500

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,29 @@
+/*
+
+function mostrarFlores(){
+   document.getElementById('abuelita').classList.add('display-none');
+   document.getElementById('flores').classList.remove('display-none');
+   
+}
+function mostrarAbuelita(){
+   document.getElementById('flores').classList.add('display-none');
+   document.getElementById('abuelita').classList.remove('display-none');
+}
+*/
+
+
+/*
+function mostrarFlores(){
+    let urlImagen = "./images/imagen-1.jpg";
+    document.getElementById('flores').setAttribute('src', urlImagen);
+}
+function mostrarAbuelita(){
+    let urlImagen = "./images/imagen-2.jpg";
+    document.getElementById('flores').setAttribute('src', urlImagen);
+}*/
+
+
+function actualizarColor(){
+    let color = document.getElementById('color').value;
+    console.log(color);
+}
